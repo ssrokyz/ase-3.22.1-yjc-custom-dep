56773f597274abcd8c97862283e25f1457511ff8
Youngjae Choi <ssrokyz@gmail.com>
2022-09-21 13:11:04 +0900

    t

--- gui/colors.py
+++ gui/colors.py
@@ -157,8 +157,13 @@ class ColorWindow:
             N = 26
         colorscale, mn, mx = self.gui.colormode_data
         if cmap == 'default':
-            colorscale = ['#{0:02X}80{0:02X}'.format(int(red))
-                          for red in np.linspace(0, 250, N)]
+            # colorscale = ['#{0:02X}80{0:02X}'.format(int(red))
+                          # for red in np.linspace(0, 250, N)]
+            import pylab as plt
+            import matplotlib
+            cmap = plt.cm.get_cmap('Reds')
+            colorscale = [matplotlib.colors.rgb2hex(c[:3]) for c in
+                          cmap(np.linspace(0, 1, N))]
         elif cmap == 'old':
             colorscale = ['#{0:02X}AA00'.format(int(red))
                           for red in np.linspace(0, 230, N)]
